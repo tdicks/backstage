e3551b59827cd0b7853c14c5316fed726b9f5578
Tom Dicks <a_fruitcake@hotmail.co.uk>
2026-07-29 23:13:56 +0100

    Disable autocomplete on search text inputs

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,17 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'bg-white px-3 py-2 text-gray-900 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+@php
+	$inputType = (string) $attributes->get('type', 'text');
+	$defaults = [
+		'class' => 'bg-white px-3 py-2 text-gray-900 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm',
+	];
+
+	if ($inputType === 'search') {
+		$defaults['autocomplete'] = 'off';
+		$defaults['autocorrect'] = 'off';
+		$defaults['autocapitalize'] = 'off';
+		$defaults['spellcheck'] = 'false';
+	}
+@endphp
+
+<input @disabled($disabled) {{ $attributes->merge($defaults) }}>
